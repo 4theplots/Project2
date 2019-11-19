7611cdaa18900e3fb0c0d2940194589565f55a90
Joshua Bugryn <46905032+bugryn-josh@users.noreply.github.com>
2019-11-19 15:16:07 -0500

    Update parse.py

--- parse.py
+++ parse.py
@@ -1,18 +1,25 @@
-f = open("Node1.txt", "r")
-
-nextPacket = True
+f = open("N1Filter.txt", "r")
 
 
 x = f.readline().strip()
-x = f.readline().strip()
-x = f.readline().strip()
-x = f.readline().strip()
 
-while(nextPacket):
-    line = x.split(" ")
+while(x != ""):
+    if(x != "@"):
+        break
+    
+
+    
+    f.readline()
+    x = f.readline().strip()
+    line = ' '.join(x.split()).split(' ')
     print(line)
-    destMac = line[2] + ":" + line[3] + ":" + line[4] + ":" + line[5] + ":" + line[6] + ":" + line[7]
-    srcMac = line[8] + ":" + line[9] + ":" + line[10] + ":" + line[11] + ":" + line[12] + ":" + line[13]
+
+    x = f.readline().strip()
+    line = ' '.join(x[6:].split()).split(' ')
+    print (line)
+
+    destMac = line[0] + ":" + line[1] + ":" + line[2] + ":" + line[3] + ":" + line[4] + ":" + line[5]
+    srcMac = line[6] + ":" + line[7] + ":" + line[8] + ":" + line[9] + ":" + line[10] + ":" + line[11]
 
     #destMac = int(destMac, 16)
     #srcMac = int(srcMac, 16)
@@ -20,21 +27,58 @@ while(nextPacket):
     print('Dest Mac: ' + str(destMac))
     print('Src Mac: ' + str(srcMac))
 
-    frameLength = int((line[14] + line[15]), 16)
-
-    print('Length: ' + str(frameLength))
-
-    protocolType = line[16]
+    protocolType = line[12] + line[13]
 
     print('Type: ' + protocolType)
 
-    if(protocolType != "45"):
-        while(f.readline().strip() != ""):
-            continue
-    
-    #TODO Add functionality for good path IPv4 packet found
+    frameLength = int(line[14][1]) * 4
 
+    print('Header Length: ' + str(frameLength))
+
+## Hex line 2
     x = f.readline().strip()
+    line = ' '.join(x[6:53].split()).split(' ')
+    print (line)
+
+    totalLength = int(line[0] + line[1], 16)
+
+    print("Total Length: " + str(totalLength))
+
+    timeToLive = int(line[6], 16)
+
+    print("Time to Live: " + str(timeToLive))
+
+    sourceIP = ''
+
+    ## BUILD IP ADDRESS WITH DOTS
+    for i in range(10,14):
+        sourceIP += str(int(line[i], 16))
+        if(i == 13):
+            break
+        else:
+            sourceIP += "."
+    
+    destinationIP = str(int(line[14],16)) + '.' + str(int(line[15], 16)) + "."
+
+    ## HEX LINE 3
+    x = f.readline().strip()
+    line = ' '.join(x[6:53].split()).split(' ')
+    print (line)
+
+    destinationIP += str(int(line[0], 16)) + '.' + str(int(line[1],16))
+
+
+    print("Source IP: " + str(sourceIP))
+    print("Destination IP: " + str(destinationIP))
+
+    icmpType = int(line[3],16)
+
+    print("ICMP Type: " + str(icmpType))
+
+    seqNum = int(line[8] + line[9], 16)
+
+    print(str(seqNum))
+
 
     if(x == ""):
         nextPacket = False
